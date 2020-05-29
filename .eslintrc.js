5b633035e6799d0057f999d013e6b9a459b4d148
nog <nogueira@pm.ba.gov.br>
2020-05-29 11:07:38 -0300

    ESLint, Prettier & EditorConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "es6": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb",
+        "prettier",
+        "prettier/react",
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "prettier",
+    ],
+    "rules": {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+  }
 };
